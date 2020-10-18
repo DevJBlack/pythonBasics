37d68dd238b218b9906189b3b91f22e9d0d28e06
Devin Black <Devinjamesblack@gmail.com>
2020-10-18 08:11:53 -0600

    finished random game

--- threecupmonty.py
+++ threecupmonty.py
@@ -1,19 +1,17 @@
-example = [1,2,3,4,5,6,7]
+# example = [1,2,3,4,5,6,7]
 from random import shuffle
 
 def shuffle_list(mylist):
     shuffle(mylist)
     return mylist
 
-result = shuffle_list(example)
-print(result)
+# result = shuffle_list(example)
+# print(result)
 
-mylist = ["","O",""]
+mylist = [" ","O"," "]
 
 def player_guess():
-
-    guess=""
-
+    guess=" "
     while guess not in ["0","1","2"]:
         guess = input("Pick a number: 0,1 or 2 ")
     return int(guess)
@@ -30,7 +28,12 @@ def check_guess(mylist,guess):
         print(mylist)
 
 # INITAL LIST
-mylist = [" ","O"]
+mylist = [" ","O"," "]
+
 # SHUFFLE LIST
+mixedup_list = shuffle_list(mylist)
 # USER GUESS
+guess = player_guess()
+
 # CHECK GUESS
+check_guess(mixedup_list, guess)
