3b016d617b9c3366fcbd22976846d4696e1c9823
Devin Black <Devinjamesblack@gmail.com>
2020-10-18 10:18:04 -0600

    Other practice functions

--- args_kwargs.py
+++ args_kwargs.py
@@ -30,3 +30,27 @@ myfunnyfunc(10,20,30, fruit = "oranage", food = "eggs", animale = "dog")
 
 # *args is a tuple that we can pass as many values as we want
 # **kwargs is a dict that we can pass as many key word values into it.
+
+def myfuncc(*args):
+    luck = []
+    for item in args:
+        if item %2 == 0:
+            luck.append(item)
+        else:
+            pass
+    return(luck)
+dev = myfuncc(5,6,7,8)
+print(dev)
+
+def skyline(str):
+    test = ""
+    letterTest = 1
+    for sky in str:
+        if letterTest % 2 ==0:
+            test += sky.upper()
+        else: 
+            test += sky.lower()
+        letterTest += 1 
+    return test
+line = skyline("Anthropomorphism")
+print(line)
